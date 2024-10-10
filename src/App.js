4afe8167629be638c80e6125aeda5d58ee63123b
sriharisbhat <srihari.bhat@robosoftin.com>
2024-10-10 19:08:14 +0530

    add table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+const [photos, setPhotos]= useState([]);
+  useEffect(()=>{
+
+    fetch("https://jsonplaceholder.typicode.com/photos")
+    .then(response => response.json())
+    .then(data => {
+      console.log("data",data);
+      setPhotos(data)
+    })
+  },[])
+
+console.log("photos", photos)
+  
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="table-container">
+        <div className="row">
+            <div className="cell">Name</div>
+            <div className="cell">Title</div>
+            <div className="cell">Images</div>
+        </div> 
+      {photos.slice(0,10).map(photo => (
+         <div className="row" key={photo.id}>
+          <div className="cell">{photo.id}</div>
+          <div className="cell">{photo.title}</div>
+          <div className="cell"><img src={photo.thumbnailUrl} alt={photo.title}/></div>
+        </div>
+    ))} 
+    </div>
+
       </header>
     </div>
   );
